600b11d149a6d3919fae1acc4b049d9e1c89fab0
jhonatan <jhoalparo1991@gmail.com>
2022-01-22 18:54:50 -0500

    chunkArrayInGroup

---
+++ chunkArrayInGroups.js
@@ -0,0 +1,17 @@
+function chunkArrayInGroups(arr, size) {
+
+
+    let newArray = [];
+    for(let i = 0; i < arr.length; i += size){
+        newArray.push(arr.slice(i, i + size));
+
+    }
+    
+  return newArray;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
